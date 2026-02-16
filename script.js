77a4b407ab88bdf002f19918fb998b60201570cc
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-02-16 15:48:47 +0000

    Create and triangulate tests for reverseString().

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-export { capitalize };
+export { capitalize, reverseString };
 
 function capitalize(string) {
   if (typeof string === "string") {
     return string.slice(0, 1).toUpperCase() + string.slice(1);
+  } else {
+    throw new Error("Parameter must be a string.");
+  }
+}
+function reverseString(string) {
+  if (typeof string === "string") {
+    return string.split("").reverse().join("");
   } else {
     throw new Error("Parameter must be a string.");
   }
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./script";
+import { capitalize, reverseString } from "./script";
 
 test("string returns with capitalised first character", () => {
   expect(capitalize("banana")).toBe("Banana");
@@ -6,4 +6,12 @@ test("string returns with capitalised first character", () => {
 });
 test("capitalize() throws error.", () => {
   expect(() => capitalize(5)).toThrow(Error);
+});
+
+test("string is reversed", () => {
+  expect(reverseString("banana")).toBe("ananab");
+  expect(reverseString("orange")).toBe("egnaro");
+});
+test("reverseString() throws error.", () => {
+  expect(() => reverseString(5)).toThrow(Error);
 });
